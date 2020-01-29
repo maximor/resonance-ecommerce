2bbf87400a051140c99e859f96387de361c5ec2f
Maximo Rodriguez <maximo1225@gmail.com>
2020-01-29 17:53:23 -0400

    adding extra configuration

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,6 +2,7 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
-        'endtoend/shopProtractorSpec.js'
+        'endtoend/shopProtractorSpec.js',
+        'endtoend/homeProtractorSpec.js'
     ]
 }
